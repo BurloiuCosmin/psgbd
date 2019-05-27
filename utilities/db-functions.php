a5e42c4b57cf7d0cb3311e9ba69d3274fd3f323a
mariahlusneac <mariahlusneac0@gmail.com>
2019-05-28 01:36:45 +0300

    hei

--- utilities/db-functions.php
+++ utilities/db-functions.php
@@ -1,5 +1,32 @@
 <?php
+
+function login($username, $password) {
+	$sql = 'BEGIN :result := login(:p_username, :p_parola); END;';
+	$stid = oci_parse($conn, $sql);
+	oci_bind_by_name($stid, ':p_username', $username);
+	oci_bind_by_name($stid, ':p_parola', $password);
+	oci_bind_by_name($tid, ':result', $result);
+	return $result;
+}
+
+
+function register($username, $password, $lastname, $firstname, $phone, $email, $adress) {
+	$sql = 'BEGIN register(:p_username, :p_parola, :p_nume, :p_prenume, :p_telefon, :p_email, :p_adresa); END;';
+	$stid = oci_parse($conn, $sql);
+	oci_bind_by_name($stid, ':p_username', $username);
+	oci_bind_by_name($stid, ':p_parola', $password);
+	oci_bind_by_name($stid, ':p_nume', $lastname);
+	oci_bind_by_name($stid, ':p_prenume', $firstname);
+	oci_bind_by_name($stid, ':p_telefon', $phone);
+	oci_bind_by_name($stid, ':p_email', $email);
+	oci_bind_by_name($stid, ':p_adresa', $adress);
+}
+
+
+
+
 /**
  * Created by PhpStorm.
  * User: cosmin
  * Date: 27
+?>
