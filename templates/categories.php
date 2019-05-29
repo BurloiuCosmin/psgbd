c080410b9ec592ca4e18c39622a5312523348119
mariahlusneac <48358732+mariahlusneac@users.noreply.github.com>
2019-05-30 02:45:05 +0300

    Update categories.php

--- templates/categories.php
+++ templates/categories.php
@@ -1,102 +1,22 @@
 <?php require_once '../template-parts/header-in.php';
+include '../utilities/connection.php';
 ?>
 
 <section class="section">
-    <div class="container-categories-list">
-        <div class="notification is-centered">
+    <div class="container-list">
+        <div class="notification">
 
-            <p class="has-text-black has-text-centered"><strong>List of available game categories. Choose your poison</strong></p>
+            <p class="has-text-black has-text-centered"><strong>Available games, while supply lasts</strong></p>
             <br>
-            <div class="columns">
-                <div class="column is-one-third">
-                    <figure class="image is-5by3">
-                        <a href="category.php?category=RTS">
-                            <img src='https://www.geek.com/wp-content/uploads/2016/03/real-time-strategy-625x350.jpg'>
-                            <p class="is-overlay is-size-2 has-text-white-bis has-text-centered">RTS</p>
-                        </a>
-                    </figure>
-                </div>
 
-                <div class="column is-one-third">
-                    <figure class="image is-5by3">
-                        <a href="category.php?category=FPS">
-                            <img src='https://i2.wp.com/culturedvultures.com/wp-content/uploads/2019/02/Rage-2.jpg?fit=1200%2C675&ssl=1'>
-                            <p class="is-overlay is-size-2 has-text-white-bis has-text-centered">FPS</p>
-                        </a>
-                    </figure>
-                </div>
+            <?php include '../model/show-one-category.php'; ?>
 
-                <div class="column is-one-third">
-                    <figure class="image is-5by3">
-                        <a href="category.php?category=fighting">
-                            <img src='https://images-eds-ssl.xboxlive.com/image?url=8Oaj9Ryq1G1_p3lLnXlsaZgGzAie6Mnu24_PawYuDYIoH77pJ.X5Z.MqQPibUVTcPtB7yoM7YK7A6apJBrPMegD8P2blNMraGLQbUdWTEHS3HGZhsNZzrnVDtyAazAMVJOLuCXhja4Sd8EHtfeOPMEc1gugt.wLdfNeLT5H3JwQxqWbWru.xjlb5vVzTHDfjVC2ZUYCLjIAbWwDg_xe7kqMR_CVplegl3gArdMNc0XU-&h=1080&w=1920&format=jpg'>
-                            <p class="is-overlay is-size-2 has-text-white-bis has-text-centered">Arcade Fighting</p>
-                        </a>
-                    </figure>
-                </div>
-            </div>
 
-            <div class="columns">
-                <div class="column is-one-third">
-                    <figure class="image is-5by3">
-                        <a href="category.php?category=TPS">
-                            <img src='https://www.gamersdecide.com/sites/default/files/styles/news_images/public/content-images/news/2016/06/11/best-3rd-person-shooters-play-2016/maxresdefault.jpg'>
-                            <p class="is-overlay is-size-2 has-text-white-bis has-text-centered">Third Person Shooter</p>
-                        </a>
-                    </figure>
-                </div>
+            
 
-                <div class="column is-one-third">
-                    <figure class="image is-5by3">
-                        <a href="category.php?category=RPG">
-                            <img src='https://www.pcgamesn.com/wp-content/uploads/2018/04/WoW-Battle-for-Azeroth-900x507.jpg'>
-                            <p class="is-overlay is-size-2 has-text-white-bis has-text-centered">RPG</p>
-                        </a>
-                    </figure>
-                </div>
-
-                <div class="column is-one-third">
-                    <figure class="image is-5by3">
-                        <a href="category.php?category=racing">
-                            <img src='https://data2.origin.com/content/dam/originx/web/app/games/need-for-speed/need-for-speed/screenshots/nfs-16/1033730_screenhi_930x524_BRZ_Style.jpg'>
-                            <p class="is-overlay is-size-2 has-text-white-bis has-text-centered">Racing</p>
-                        </a>
-                    </figure>
-                </div>
-            </div>
-
-            <div class="columns">
-                <div class="column is-one-third">
-                    <figure class="image is-5by3">
-                        <a href="category.php?category=sports">
-                            <img src='https://i.gadgets360cdn.com/large/fifa-19-review_1537859706679.jpg'>
-                            <p class="is-overlay is-size-2 has-text-white-bis has-text-centered">Sports</p>
-                        </a>
-                    </figure>
-                </div>
-
-                <div class="column is-one-third">
-                    <figure class="image is-5by3">
-                        <a href="category.php?category=adventure">
-                            <img src='https://www.clicktechtips.com/wp-content/uploads/2017/10/maxresdefault-1.jpg'>
-                            <p class="is-overlay is-size-2 has-text-white-bis has-text-centered">Adventure</p>
-                        </a>
-                    </figure>
-                </div>
-
-                <div class="column is-one-third">
-                    <figure class="image is-5by3">
-                        <a href="category.php?category=action">
-                            <img src='https://i.kinja-img.com/gawker-media/image/upload/s--gwesdJBo--/c_scale,dpr_2.0,f_auto,fl_progressive,q_80,w_800/noicn5tlqv60kfccqzem.jpg'>
-                            <p class="is-overlay is-size-2 has-text-white-bis has-text-centered">Action</p>
-                        </a>
-                    </figure>
-                </div>
-            </div>
 
         </div>
     </div>
 </section>
-
 
 <?php require_once '../template-parts/footer.php';  ?>
