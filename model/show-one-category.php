d41983ef99bc69df097f6845c231752b9be105aa
mariahlusneac <mariahlusneac0@gmail.com>
2019-05-30 02:04:21 +0300

    jale

--- model/show-one-category.php
+++ model/show-one-category.php
@@ -1,33 +1,46 @@
 <?php
 require_once '../utilities/connection.php';
 require_once '../utilities/db-functions.php';
-
 $ok = 0;
 $requested_category = $_GET['category'];
-$categories         = array(
-	'actiune'   => 'action',
-	'aventura'  => 'adventure',
-	'lupte'     => 'fighting',
-	'puzzle'    => 'puzzle',
-	'curse'     => 'racing',
-	'RPG'       => 'RPG',
-	'sport'     => 'sports',
-	'strategie' => 'RTS',
-	'FPS'       => 'FPS',
-	'TPS'       => 'TPS'
-);
+$categories         = array('action', 'adventure', 'fighting', 'puzzle', 'racing', 'RPG', 'sports', 'RTS', 'FPS', 'TPS');
 
-foreach ( $categories as $key => $category ) {
+// foreach ($categories as $category) {
 
-	if ( $requested_category == $category ) {
+// 	if ($requested_category == $category) {
+// 		$ok                 = 1;
+// 		$requested_category = $category;
+// 		//$games = filtrareDupaCategorii( $requested_category );
+		$conn = oci_connect('MARIUTA', 'MARIUTA', "//localhost:1521");
+		$sql = 'SELECT * FROM jocuri';
+		$stid = oci_parse($conn, $sql);
+		if (!$stid) {
+			$e = oci_error($conn);
+			trigger_error(htmlentities($e['message'], ENT_QUOTES), E_USER_ERROR);
+		}
+		//oci_bind_by_name($stid, ':categorie', $requested_category);
+		$r = oci_execute($stid);
+		if (!$r) {
+			$e = oci_error($stid);
+			trigger_error(htmlentities($e['message'], ENT_QUOTES), E_USER_ERROR);
+		}
+		print "<table border='1'>\n";
+		while ($row = oci_fetch_array($stid, OCI_ASSOC + OCI_RETURN_NULLS)) {
+			print "<tr>\n";
+			foreach ($row as $item) {
+				print "    <td>" . ($item !== null ? htmlentities($item, ENT_QUOTES) : "&nbsp;") . "</td>\n";
+			}
+			print "</tr>\n";
+		}
+		print "</table>\n";
+// 	}
+// }
 
-		$ok                 = 1;
-		$requested_category = $key;
-		$games              = filtrareDupaCategorii( $requested_category );
-	}
-}
 
-
-if( $ok == 0 ){
-	header( 'location: ../templates/categories.php' );
-}
+// if ($ok == 0) {
+// 	header('location: ../templates/categories.php');
+// 	=== === =
+// 	if (in_array($requested_category, $categories)) { } else {
+// 		header('location: ../templates/categories.php');
+// 		>> >> >> > Stashed changes
+// 	}
