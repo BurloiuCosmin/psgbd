dfc5e505f4508a54a27cc657433c1a3364b6ca0b
mariahlusneac <mariahlusneac0@gmail.com>
2019-05-30 04:43:39 +0300

    cart

--- model/add-to-cart.php
+++ model/add-to-cart.php
@@ -2,9 +2,13 @@
 require_once '../utilities/connection.php';
 require_once '../utilities/db-functions.php';
 
-$game = $_GET['id'];
+$id_game = $_GET['id'];
+$id_client = $_COOKIE['id_client'];
 $conn = oci_connect('MARIUTA', 'MARIUTA', "//localhost:1521");
-
-
+$sql4 = 'begin adaugareInCos(:p_id_client, :p_id_joc); end;';
+$stid4= oci_parse($conn, $sql4);
+oci_bind_by_name($stid4, ':p_id_client', $id_client);
+oci_bind_by_name($stid4, ':p_id_joc', $id_game);
+oci_execute($stid4);
 //pe baza lui faci query
 
