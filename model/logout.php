50701824a1e5ce7af45e7ff2d80b215fea085d66
Cosmin Burloiu <cosmin@pixelgrade.com>
2019-05-30 05:34:57 +0300

    yes v2

--- model/logout.php
+++ model/logout.php
@@ -1,10 +1,9 @@
 <?php
 
 if(isset($_POST['logout'])) {
-	setcookie( "first_name", '', time() - 1, '/' );
-	setcookie( "email", '', time() - 1, '/' );
+	setcookie( "username", '', time() - 1, '/' );
 	setcookie( "password", '', time() - 1, '/' );
-	setcookie( "user_id", '', time() - 1, '/' );
+	setcookie( "id", '', time() - 1, '/' );
 
 	header( 'location: ../templates/login.php' );
 	die;
